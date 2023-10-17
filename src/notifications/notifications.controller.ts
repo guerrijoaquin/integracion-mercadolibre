dfb95ae2f38647a63601f5ec3d82b4d4301bde57
Joaquin Guerrini <joaquinguerrini1@gmail.com>
2023-10-17 13:52:20 -0300

    fix: añadido filtro de IP para notificaciones de mercadolibre

--- src/notifications/notifications.controller.ts
+++ src/notifications/notifications.controller.ts
@@ -1,15 +1,16 @@
-import { Body, Controller, Post, Res } from '@nestjs/common';
+import { Body, Controller, Post, Res, UnauthorizedException } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { Response } from 'express';
 import { RealIP } from 'nestjs-real-ip';
 
 @Controller('notifications')
 export class NotificationsController {
+  private MELI_IPS = ['54.88.218.97', '18.215.140.160', '18.213.114.129', '18.206.34.84'];
   constructor(private readonly notificationsService: NotificationsService) {}
 
   @Post()
   async receiveNotification(@RealIP() ip: string, @Body() payload, @Res() res: Response) {
-    console.log('Notification from IP: ', ip);
+    if (!this.MELI_IPS.includes(ip)) throw new UnauthorizedException(`Unknow IP: ${ip}`);
     await this.notificationsService.handleNotification(payload);
     return res.status(200).send('Ok');
   }
